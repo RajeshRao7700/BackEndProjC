806f2199c6374f40d3332a487f48e0311eaa2757
Rajesh Rao <raorajesh7700@gmail.com>
2026-04-15 15:39:34 +0530

    added backend basics

---
+++ src/test/java/com/BackendProj/BackendProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BackendProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
